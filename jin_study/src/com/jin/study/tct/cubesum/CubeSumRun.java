092c458b3f88897730b150e8c01caa80d9cec2f6
sndream1371 <sndream1371@gmail.com>
2019-05-16 14:23:13 +0900

    2차월 배열

---
+++ jin_study/src/com/jin/study/tct/cubesum/CubeSumRun.java
@@ -0,0 +1,17 @@
+package com.jin.study.tct.cubesum;
+
+public class CubeSumRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int [][] cube = {{5,7,8},{4,2,1},{2,5,6}};
+
+		CubeSum cubeSum = new CubeSum();
+		cubeSum.sum(cube);
+		
+		
+	}
+
+}
