7f2eac3c3cbc0480af472a2df8644d1febcbd229
sndream1371 <sndream1371@gmail.com>
2019-06-17 10:36:59 +0900

    거스름돈을 몇 개의 동전으로 지불할 수 있는지를 계산하는 문제

--- jin_study/src/com/jin/study/javajungsuk/Array_Exercise5_6.java
+++ jin_study/src/com/jin/study/javajungsuk/Array_Exercise5_6.java
@@ -22,6 +22,7 @@ class Array_Exercise5_6 {
 		int[] coinUnit = { 500, 100, 50, 10 };
 		int money = 2680;
 		System.out.println("money=" + money);
+		
 		for (int i = 0; i < coinUnit.length; i++) {
 			/* 알맞은 코드를 넣어 완성하시오 (1) . */
 			
