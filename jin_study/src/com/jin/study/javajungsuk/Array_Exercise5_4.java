9bb7499d286877cc09898f36824b700105ea58db
sndream1371 <sndream1371@gmail.com>
2019-06-17 10:23:48 +0900

    배열 에 담긴 모든 값의 총합과 평균

--- jin_study/src/com/jin/study/javajungsuk/Array_Exercise5_4.java
+++ jin_study/src/com/jin/study/javajungsuk/Array_Exercise5_4.java
@@ -1,12 +1,15 @@
 package com.jin.study.javajungsuk;
 
 /*
- * 
  *  차원 배열 에 담긴 모든 값의 총합과 평균을 구하는 프로그램을 완성하시오
  */
 class Array_Exercise5_4 {
 	public static void main(String[] args) {
-		int[][] arr = { { 5, 5, 5, 5, 5 }, { 10, 10, 10, 10, 10 }, { 20, 20, 20, 20, 20 }, { 30, 30, 30, 30, 30 } };
+		int[][] arr = { {  5,  5,  5,  5,  5 }, 
+				        { 10, 10, 10, 10, 10 }, 
+				        { 20, 20, 20, 20, 20 }, 
+				        { 30, 30, 30, 30, 30 } 
+				      };
 		int total = 0;
 		float average = 0;
 		
@@ -25,7 +28,9 @@ class Array_Exercise5_4 {
 
 		//-----------------------------------------------------------
 		
+		System.out.println("cnt=" + cnt);
 		System.out.println("total=" + total);
 		System.out.println("average=" + average);
+		
 	} // end of main
 } // end of class
