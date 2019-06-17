1d479ceb3686a402fc7ba03d4ae62d6f5f83826d
sndream1371 <sndream1371@gmail.com>
2019-06-17 15:49:58 +0900

    ArrayList를 이용하여 워드를 찾아 카운트를 한다.

--- jin_study/src/com/jin/study/quiz/WordCounting.java
+++ jin_study/src/com/jin/study/quiz/WordCounting.java
@@ -27,23 +27,24 @@ public class WordCounting {
 		government 2
 	 * 
 	 * 
-	 * 
+	 *  ArrayList 오름차순 내림차순 정렬 
 	 * 
 	 */
 	public static void main(String[] argv) {
 		String inputMsg="As the country became embroiled in a domestic crisis, the first government was dislodged and succeeded by several different administrations. Bolikango served as Deputy Prime Minister in one of the new governments before a partial state of stability was reestablished in 1961. He mediated between warring factions in the Congo and briefly served once again as Deputy Prime Minister in 1962 before returning to the parliamentary opposition. After Joseph-Desire Mobutu took power in 1965, Bolikango became a minister in his government. Mobutu soon dismissed him but appointed him to the political bureau of the Mouvement Populaire de la Revolution. Bolikango left the bureau in 1970. He left Parliament in 1975 and died seven years later. His grandson created the Jean Bolikango Foundation in his memory to promote social progress. The President of the Congo posthumously awarded Bolikango a medal in 2005 for his long career in public service.";
 
 		Map<String, Integer> hMap = new HashMap<>();
-		inputMsg=inputMsg.replaceAll("[^0-9a-zA-Z ]","" );
-		System.out.println(inputMsg);
+		inputMsg=inputMsg.replaceAll("[^0-9a-zA-Z ]","" ); //숫자 영문자를 제외한 나머지 문자를 삭제 (단어 카운트를 위함)
+		System.out.println("After replaceAll >"+inputMsg);
 		
-		String[] arrInputMsg =inputMsg.split(" ");
+		String[] arrInputMsg =inputMsg.split(" "); //공백으로 단어 구분
 		Arrays.sort(arrInputMsg); //오름차순 정렬
 		//System.out.println(arrInputMsg);
+		
 		int cnt=1;
 		for(int i=0; i < arrInputMsg.length; i++) {
 			if(arrInputMsg.length > i+1) {
-				if(arrInputMsg[i].equals(arrInputMsg[i+1])) {
+				if(arrInputMsg[i].equals(arrInputMsg[i+1])) { //정렬된 문자를 앞뒤 순서대로 비교하여 같으면 카운트 +1 한다.
 					cnt++;
 				}else {
 					hMap.put(arrInputMsg[i], cnt);
@@ -53,25 +54,33 @@ public class WordCounting {
 			
 		}
 		
-       List<String> keySetList = new ArrayList<>(hMap.keySet());
-        // 내림차순 //
-        Collections.sort(keySetList, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return hMap.get(o2).compareTo(hMap.get(o1));
-            }
-        });
-         
-        // 오름차순 //
-        //Collections.sort(keySetList, (o1, o2) -> (hMap.get(o1).compareTo(hMap.get(o2))));
-        
-        for(String key : keySetList) {
-            System.out.println(String.format("Key : %s, Value : %s", key, hMap.get(key)));
-        }
-        System.out.println("-----------------------------------------------------------------");
-
+		List<String> keySetList = new ArrayList<>(hMap.keySet());
+		
+		//ArrayList 내림차순 //
+		Collections.sort(keySetList, new Comparator<String>() {
+			@Override
+	        public int compare(String o1, String o2) {
+	        	return hMap.get(o2).compareTo(hMap.get(o1));
+	        }
+		});
+	     
+		//ArrayList 오름차순 //
+		//Collections.sort(keySetList, (o1, o2) -> (hMap.get(o1).compareTo(hMap.get(o2))));
+		
+		
+		//결과 출력
+		int disCnt =0;
+		for(String key : keySetList) {
+			disCnt++;
+			if( disCnt == 11 ) { //10개까지만 출력
+				break;
+			}else {
+				System.out.println(String.format("Key : %s, Value : %s", key, hMap.get(key)));
+			}
+		}
+		
+		System.out.println("-----------------------------------------------------------------");
+	
 	}
-	
-	
 
 }
