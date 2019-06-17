f45a0e214c68d9de02b7bf24e48aaa51cb522185
sndream1371 <sndream1371@gmail.com>
2019-06-17 11:08:56 +0900

    배열을 시계방향으로 90도 회전시켜서 출력

--- jin_study/src/com/jin/study/javajungsuk/Array_Exercise5_9.java
+++ jin_study/src/com/jin/study/javajungsuk/Array_Exercise5_9.java
@@ -1,16 +1,18 @@
 package com.jin.study.javajungsuk;
 
 /*
- * 주어진 배열을 시계방향으로 도 회전시켜서 출력하는 프로그램을 완성하시오 
+ * 주어진 배열을 시계방향으로 90도 회전시켜서 출력하는 프로그램을 완성하시오 
  * 
  * [실행결과]
+ * **
  * ** 
  * ** 
  * ***** 
  * *****
  * 
-   **** 
-   **** 
+ * [결과]
+   ***** 
+   ***** 
    ** 
    **
    **
@@ -23,10 +25,11 @@ class Array_Exercise5_9 {
 				          { '*', '*', ' ', ' ', ' ' }, 
 				          { '*', '*', '*', '*', '*' },
 				          { '*', '*', '*', '*', '*' } 
-				        }; //[4][5]
-		char[][] result = new char[star[0].length][star.length]; // [5],[4]
+				        }; //[5][5]
+		char[][] result = new char[star.length][star[0].length]; // [5],[5]
 		System.out.println("star[0].length >"+star[0].length +" star.length >"+star.length);
 		
+		//입력값 출력
 		for (int i = 0; i < star.length; i++) {
 			for (int j = 0; j < star[i].length; j++) {
 				System.out.print(star[i][j]);
@@ -35,12 +38,14 @@ class Array_Exercise5_9 {
 		}
 		System.out.println();
 
+		
+		//결과출력
 		for (int i = 0; i < result.length; i++) {
 			for (int j = 0; j < result[i].length; j++) {
 				
-				//System.out.println("star[4-j][i] > "+star[4-j][i]);
-				result[i][j] = star[4-j][i];
-						
+				//result[i][j] = star[j][4-i];  //왼쪽으로 90도 돌리기
+				result[i][j] = star[4-j][i];  //오른쪽으로 90도 돌리기				
+				
 				System.out.print(result[i][j]);
 			}
 			System.out.println();
