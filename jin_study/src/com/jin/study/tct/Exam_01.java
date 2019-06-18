7f256c7b10f75fca3539343af12e42270b1d0a66
sndream1371 <sndream1371@gmail.com>
2019-06-18 16:25:04 +0900

    알파벳 순서대로 정렬하고 아파벳 다음 갯수를 입력

--- jin_study/src/com/jin/study/tct/Exam_01.java
+++ jin_study/src/com/jin/study/tct/Exam_01.java
@@ -11,14 +11,13 @@ import java.util.Scanner;
  * 입력받은 문자열을 대문자를 소문자로 변환한다.
  * 알파벳 순서대로 정렬하고 아파벳 다음 갯수를 입력한다.  ex) aaaACCCcbbbB - > a4b4c4
  * 
- *  
- * 
  */
 public class Exam_01 {
 
 	public static void main(String[] argv) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Input value >>>");
+		
 		String msg = sc.nextLine(); //키보드 문자입력
 		System.out.println("Input message >"+msg);
 		
@@ -28,8 +27,8 @@ public class Exam_01 {
 		arrChar = (msg.toLowerCase()).toCharArray();
 		
 		for(int i=0; i < arrChar.length; i++) {
-			System.out.println("array >"+arrChar[i]);
-			arrList.add(arrChar[i]+"");
+			//System.out.println("array >"+arrChar[i]);
+			arrList.add( arrChar[i]+"" ); //String.valueOf()
 		}
 		
 		Collections.sort(arrList); //입력값 정렬 , 주석시 입력순서대로 문자열 압축해서 카운드, 주석해제시 전체문자열 sort해서 문자별로 카운트
@@ -37,20 +36,19 @@ public class Exam_01 {
 		int cnt=0;
 		String display="", tempStr="";
 		
-		for(String strArr : arrList) {
+		for(String data : arrList) {
 			
 			//System.out.println("tempStr >"+tempStr);
-			if(!tempStr.equals(strArr)) {
-				tempStr = strArr;
+			if(!tempStr.equals(data)) {
+				tempStr = data;
 				
 				for(int j=0; j < arrList.size(); j++) {
 					//System.out.println("Final result sort strArr :"+strArr+" arrList(j):"+arrList.get(j));
-					if( strArr.equals(arrList.get(j))) {
+					if( data.equals(arrList.get(j))) {
 						display = cnt++ + "";
 					}
 				}
-				display = strArr + cnt;
-				//System.out.println(display);
+				display = data + cnt;
 				System.out.print(display);
 				cnt=0;
 			}
