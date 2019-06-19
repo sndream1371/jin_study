54b27d242c665ce52e46804aae379322eb81d77f
sndream1371 <sndream1371@gmail.com>
2019-06-19 15:14:21 +0900

    (진영풀이) 파일에 정보를읽어 휴면계좌의 합과, 은행별 휴면계좌합이 가장큰 은행을 찾는다.

--- jin_study/src/com/jin/study/tct/Exercise_02.java
+++ jin_study/src/com/jin/study/tct/Exercise_02.java
@@ -1,9 +1,5 @@
 package com.jin.study.tct;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -11,26 +7,27 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Scanner;
 
 /*
  * 
  * 입력으로 제공되는 휴면계좌의 정보는 다음과 같다. ※ 동명이인은 없다. ※ 은행이름은 대문자 “A”, “B”, “C” 3개만으로 구성된다.
  * 이름 휴면 계좌번호 은행이름 잔액
- *  HONG 123-001-2345 A 23000 
- *  KIM 03-002-6766 B 14000
- *  PARK 09712-003-55432 C 5400 
- *  MOON 09143-001-4322 A 10000
-    KIM 020-001-1998 A 1200 
-    HONG 02267-003-12788 C 63000
-     KIM 091-001-2511 A 8900
+[INPUT FILE 내용] - java_input_data.txt
+
+HONG	123-001-2345	A	23000
+KIM	03-002-6766	B	14000
+PARK	9712341234	C	5400
+MOON	12-1234-1234	A	10000
+KIM	535-234-2344	A	1200
+HONG	2345-2345-234	C	63000
+KIM	234-445-452	A	8900
 
 
 2. 사람의 이름이 입력으로 주어졌을 때 해당 사람의 모든 휴면 계좌를 찾아 그 잔액의 합을 구한다. [입력된 사람이름] KIM
 [휴면계좌의 잔액 합계] 14000 + 1200 + 8900 = 24100
+
 3. 은행별 휴면계좌의 잔액 합이 가장 큰 은행의 이름을 검색한다. ※ 은행별 휴면계좌의잔액 합이 동일한 경우는 없다.
 A은행의 휴면계좌 잔액 합 : 23000 + 10000 + 1200 + 8900 = 43100 B은행의 휴면계좌 잔액 합 : 14000 C은행의 휴면계좌 잔액 합 : 5400 + 63000 = 68400
 [휴면계좌 잔액 합이 가장 큰 은행] C
@@ -45,11 +42,12 @@ public class Exercise_02 {
 
 	public static void main(String[] argv) {
 		
-		System.out.println("account sum :"+accSum());
+		System.out.println("account sum :"+personAccountSum("KIM")); //사람의 이름이 입력으로 주어졌을 때 휴면 계좌를 찾아 그 잔액의 합
 		System.out.println("bank account sum :"+bankAccountSum());
 
 	}
 
+	//은행별 휴면계좌의 잔액 합이 가장 큰 은행의 이름을 검색
 	public static String bankAccountSum() {
 		List<String> list = fileReader();
 		String [] arrAcc= null;
@@ -81,14 +79,15 @@ public class Exercise_02 {
 	}
 
 	
-	public static int accSum() {
+	//사람의 이름이 입력으로 주어졌을 때 휴면 계좌를 찾아 그 잔액의 합
+	public static int personAccountSum(String person) {
 		List<String> list = fileReader();
 		String [] arrAcc= null;
 		int sum=0;
-		for(String obj : list) {
-			arrAcc = obj.split("\\t");
+		for(String data: list) {
+			arrAcc = data.split("\\t");
 			if(arrAcc != null) {
-				if(arrAcc[0].equals("KIM")) sum += Integer.parseInt(arrAcc[3]);
+				if(arrAcc[0].equals(person)) sum += Integer.parseInt(arrAcc[3]);
 			}
 		}
 		
@@ -97,12 +96,16 @@ public class Exercise_02 {
 	}
 	
 	public static List<String> fileReader() {
+		
         //파일 객체 생성
         Path path = Paths.get("C:\\Users\\sndre\\Documents\\TCT_EAD CNS시험\\test_data\\java_input_data.txt");
+        
         // 캐릭터셋 지정
         Charset cs = StandardCharsets.UTF_8;
+        
         //파일 내용담을 리스트
         List<String> list = new ArrayList<String>();
+        
         try{
             list = Files.readAllLines(path,cs);
         }catch(IOException e){
@@ -110,9 +113,9 @@ public class Exercise_02 {
         }
         
         //just print
-        for(String readLine : list){
-            System.out.println(readLine);
-        }
+//        for(String readLine : list){
+//            System.out.println(readLine);
+//        }
         
         return list;
 
