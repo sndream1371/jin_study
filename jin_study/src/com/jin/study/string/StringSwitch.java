12790060a9cbb77d16be88bffb80f3d05edb3619
sndream1371 <sndream1371@gmail.com>
2019-09-03 14:07:50 +0900

    ..

--- jin_study/src/com/jin/study/string/StringSwitch.java
+++ jin_study/src/com/jin/study/string/StringSwitch.java
@@ -4,13 +4,14 @@ public class StringSwitch {
 
 	public static void main(String[] args) {
 		
-		int comp = 4; //값을 다르게 해서 실행하면 1~5까지 입력하면 5가 출력,  6~9까지 입력하면 9출력 된다.
+		int comp = 45; //값을 다르게 해서 실행하면 1~5까지 입력하면 5가 출력,  6~9까지 입력하면 9출력 된다.
 		
 		switch( comp ) {
 			case 1 :
 			case 2 :
 			case 3 :
 			case 4 :
+			case 45 :
 			case 5 :
 				System.out.println("this is 5");
 				break;
